c08180d27086dec159f45af35be134747e4cdf25
Jeancy MUPUANGA LUMBANZILA <36196471+Skyrix91@users.noreply.github.com>
2021-05-26 13:41:11 +0200

    Add files via upload

--- Template Asian Home Paris/Backend/objects/Client.java
+++ Template Asian Home Paris/Backend/objects/Client.java
@@ -1,5 +1,7 @@
 package objects;
 
+import java.util.Objects;
+
 public class Client {
     
     private String name;
@@ -15,31 +17,44 @@ public class Client {
         this.mail = mail;
         this.phoneNumber = phoneNumber;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setMail(String mail) {
+        this.mail = mail;
     }
+
     public String getMail() {
         return mail;
     }
-    public void setMail(String mail) {
-        this.mail = mail;
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+
+    @Override
+    public boolean equals(Object o) {
+        //On vérifie si les références d'objets sont identiques
+        if (this == o) return true;
+        //On s'assure que les objets sont du même type
+        if (o == null || getClass() != o.getClass()) return false;
+        //Maintenant, on compare les attributs de nos objets
+        Client client = (Client) o;
+
+        return Objects.equals(name, client.name) && Objects.equals(mail, client.mail) && Objects.equals(phoneNumber, client.phoneNumber);
     }
 
-
-
-
-    
-
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, mail, phoneNumber);
+    }
 }
--- Template Asian Home Paris/Backend/objects/Reservation.java
+++ Template Asian Home Paris/Backend/objects/Reservation.java
@@ -4,13 +4,25 @@ public class Reservation {
 
     private String userName;
     private int numOfGuests;
+    private Object LocalDate;
+    protected String date;
 
-    Reservation(int numOfGuests, String userName) {
+
+    Reservation(int numOfGuests, String userName, String date) {
         this.numOfGuests = numOfGuests;
         this.userName = userName;
+        this.LocalDate = date;
     }
 
     public String ToString() {
         return String.format("%d personnes au nom de :%s", numOfGuests,userName);
+    }
+
+    public void setReservationDate(String date){
+        this.date =date;
+    }
+
+    public String getReservationDate() {
+        return date;
     }
 }
