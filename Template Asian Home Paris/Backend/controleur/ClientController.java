1b49e99e08616b458700320d73d4cc994c390764
Jeancy MUPUANGA LUMBANZILA <36196471+Skyrix91@users.noreply.github.com>
2021-05-28 16:55:08 +0200

    Add files via upload

---
+++ Template Asian Home Paris/Backend/controleur/ClientController.java
@@ -0,0 +1,44 @@
+package controleur;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+import objects.Client;
+import service.UserService;
+
+@RestController
+public class ClientController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/getUserDetails")
+    public Client getExample(@RequestHeader() String name) {
+        return new Client (name,"frederic@gmail.com","0614297808");
+    }
+
+    @PostMapping ("/createUser")
+    public String saveUser(@RequestBody Client client) throws ExecutionException, InterruptedException {
+        return userService.saveUser(client);
+    }
+
+    @PutMapping("/updateUser")
+    public String putExample(@RequestBody Client client) {
+
+        return "Update user" + client.getName();
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteExample(@RequestHeader String name) {
+
+        return "Deleted user"+name;
+    }
+}
--- Template Asian Home Paris/Backend/service/UserService.java
+++ Template Asian Home Paris/Backend/service/UserService.java
@@ -21,4 +21,22 @@ public class UserService {
          ApiFuture<WriteResult>  collectionsApiFuture = dbFirestore.collection("client").document(client.getName()).set(client);
                  return collectionsApiFuture.get().getUpdateTime().toString();
      }
+
+     public Client getClient (String name) throws InterruptedException, ExecutionException {
+         Firestore dbFirestore = FirestoreClient.getFirestore();
+         DocumentReference docRef = db.collection("users").document("name");
+// asynchronously retrieve the document
+         ApiFuture<DocumentSnapshot> future = docRef.get();
+
+         Client client = null;
+// ...
+// future.get() blocks on response
+         DocumentSnapshot document = future.get();
+         if (document.exists()) {
+             client = document.toObject(Client.class);
+             return client;
+         } else {
+             System.out.println("No such document!");
+         }return null;
+     }
 }
