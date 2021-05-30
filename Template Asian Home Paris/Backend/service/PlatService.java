33f6ffb86b94d5409fd3e175ca36023f7b7ae2d9
Seycha Oudeinguene <36196523+Oudeinguene@users.noreply.github.com>
2021-05-30 11:44:56 +0200

    plats

--- Template Asian Home Paris/Backend/service/PlatService.java
+++ Template Asian Home Paris/Backend/service/PlatService.java
@@ -1,13 +1,29 @@
 package service;
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
-import com.google.firebase.cloud.FirestoreClient;
-import objects.Client;
+import com.google.cloud.firestore.*;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+
+@Service
 public class PlatService {
 
-}
+    private DocumentReference db;
+    private Object Plat;
+
+    public List<QueryDocumentSnapshot> getPlat () throws InterruptedException, ExecutionException {
+        // Create a reference to the cities collection
+        CollectionReference plats = db.collection("Plats");
+// Create a query against the collection.
+        Query query = plats.whereEqualTo("nom", "riz,sashimi");
+
+
+        ApiFuture<QuerySnapshot> querySnapshot = query.get();
+
+// retrieve  query results asynchronously using query.get()
+
+        return querySnapshot.get().getDocuments();
+        }
+    }
